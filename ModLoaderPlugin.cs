9bf6babd2756f3bc6160b8722fd14ef074b534dd
ConfiG <max1m1232@yandex.ru>
2020-07-14 21:50:37 +0300

    Make use of BepInEx.ConfigurationManager for a mod list (at least temporarily)

--- ModLoaderPlugin.cs
+++ ModLoaderPlugin.cs
@@ -1,21 +1,28 @@
 ﻿using BepInEx;
+using BepInEx.Bootstrap;
 
 using GOIModApis;
 
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace GOIModLoader {
     [BepInProcess("GettingOverIt.exe")]
     [BepInPlugin("GOI.plugins.modLoader", "Mod Loader", "0.1.0")]
     [BepInDependency("GOI.plugins.modApis")]
+    [BepInDependency("com.bepis.bepinex.configurationmanager")]
     public class ModLoaderPlugin : BaseUnityPlugin {
+        private ConfigurationManager.ConfigurationManager configurationManager;
         public void Awake() {
-            Debug.Log("Mod Loader initialized");
+            configurationManager = (ConfigurationManager.ConfigurationManager)
+                Chainloader.PluginInfos["com.bepis.bepinex.configurationmanager"].Instance;
             MainMenuApis.onMainMenuLoaded += (_, __) => AddModSettingsButton();
+            Debug.Log("Mod Loader initialized");
         }
         public void AddModSettingsButton() {
-            Button button = MainMenuApis.AddMainMenuButton(3, "Mod Settings");
+            Button button = MainMenuApis.AddMainMenuButton(3, "Mods");
+            button.onClick.AddListener(new UnityAction(() => configurationManager.DisplayingWindow = !configurationManager.DisplayingWindow));
         }
     }
 }
